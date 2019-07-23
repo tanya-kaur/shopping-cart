67555d3d2cfc3e8b85ca3f47a8062d8de05ccbaf
tanya-kaur <51701390+tanya-kaur@users.noreply.github.com>
2019-07-23 12:22:30 +0530

    first project

---
+++ shopping-cart/includes/footer.php
@@ -0,0 +1,49 @@
+<footer>
+        <div class="container-fluid">
+            <div class="row" style="height: 350px; margin-top: 30px; background: lightgreen;">
+                <div class="col-sm-3" style="margin-top: 30px;">
+                    <h4 class="font-weight: bolder" style="margin-bottom: 30px">Payment Methods</h4>
+                    <a href="#"> <i class="fa fa-cc-mastercard" style="font-size: 38px; color:blue"></i></a><br>
+                    <a href="#"><i class="fa fa-cc-paypal" style="font-size: 38px; color:blue"></i></a><br>
+                    <a href="#"> <i class="fa fa-cc-visa" style="font-size: 38px; color:blue"></i></a><br>
+                    <a href="#"> <i class="fa fa-cc-discover" style="font-size: 38px; color:blue"></i></a><br>
+
+                </div>
+                <div class="col-sm-3" style="margin-top: 30px;">
+                    <h4 class="font-weight: bolder" style="margin-bottom: 30px">Quick Links</h4>
+                    <button type="button" class="btn btn-link" style="color: darkblue">Help</button><br>
+                    <button type="button" class="btn btn-link" style="color: darkblue">Shipping Policy</button><br>
+                    <button type="button" class="btn btn-link" style="color: darkblue">Cancellation &
+                        Returns</button><br>
+                    <button type="button" class="btn btn-link" style="color: darkblue">FAQ</button><br>
+                    <button type="button" class="btn btn-link" style="color: darkblue">Privacy Policy</button><br>
+                    <button type="button" class="btn btn-link" style="color: darkblue">Disclamer</button><br>
+                </div>
+                <div class="col-sm-3" style="margin-top: 30px;">
+                    <h4 class="font-weight: bolder" style="margin-bottom: 30px">Lets be social</h4>
+                    <a href="#"><img src="images/facebook (1).png" style="width: 30px; height: 30px;"></a>
+                    <a href="#"><img src="images/tumblr.png" style="width: 30px; margin-left: 15px; height: 30px;"></a>
+                    <a href="#"><img src="images/google-plus.png"
+                            style="width: 30px; margin-left: 15px; height: 30px;"></a>
+                </div>
+                <div class="col-sm-3" style="margin-top: 30px;">
+                    <h4 class="font-weight: bolder" style="margin-bottom: 30px">Contact us</h4>
+                    <a href="#"><img src="images/email-black-envelope-back.png" style="width: 30px; height: 30px;"></a>
+                    <a href="mailto: gocart@gmail.com"
+                        style="text-decoration: none; margin-left: 10px; color: darkblue">gocart@gmail.com</a><br>
+                    <a href="#"><img src="images/phone-volume.png" style="width: 30px; height: 30px;"></a>
+                    <a href="tel:180000112"
+                        style="text-decoration: none;  margin-left: 10px;color: darkblue">1800001122</a>
+                </div>
+            </div>
+
+
+        </div>
+        <div class="container-fluid" style=" background: gray">
+
+            <p class="font-weight-bolder" style="margin-left: 550px; margin-bottom: 0%;">&copy; 2007-2014 Company.com
+            </p>
+        </div>
+
+    </footer>
+  
